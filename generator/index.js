cd99881dc7570b39a4bb324f2a236637b7aff05a
liekkas <zjq0717@163.com>
2019-06-01 21:06:46 +0800

    update dependency

--- generator/index.js
+++ generator/index.js
@@ -13,7 +13,7 @@ module.exports = (api, options, rootOptions) => {
   // 项目依赖
   api.extendPackage({
     dependencies: {
-      "hyz-components": "^0.6.2",
+      "hyz-components": "^0.6.4",
       iview: "^3.3.2"
     }
   });
